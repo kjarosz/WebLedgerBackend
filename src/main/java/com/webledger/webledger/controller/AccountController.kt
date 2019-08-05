f53fb38fa374dea0cf3071818305a7e20fb08f2d
Kamil Jarosz <kjarosz19946@gmail.com>
2019-08-05 18:44:59 -0400

    Add swagger to project.

--- src/main/java/com/webledger/webledger/controller/AccountController.kt
+++ src/main/java/com/webledger/webledger/controller/AccountController.kt
@@ -3,6 +3,7 @@ package com.webledger.webledger.controller
 import com.webledger.webledger.entity.Account
 import com.webledger.webledger.service.AccountService
 import com.webledger.webledger.transferobject.AccountTo
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,9 +13,11 @@ class AccountController(
         @Autowired
         val accountService: AccountService)
 {
+    @ApiOperation(value = "Get list of all accounts", response = Array<Account>::class)
     @GetMapping("/accounts")
     fun getAccounts(): ResponseEntity<Iterable<Account>?> = ResponseEntity.ok(accountService.getAllAccounts())
 
+    @ApiOperation(value = "Get account by id", response = Account::class)
     @GetMapping("/accounts/{id}")
     fun getAccount(@PathVariable("id") id: Int): ResponseEntity<Account?> {
         val account = accountService.getAccount(id)
@@ -25,6 +28,7 @@ class AccountController(
         }
     } 
 
+    @ApiOperation(value = "Save an account, new or update", response = Account::class)
     @PostMapping("/accounts/save")
     fun saveAccount(@RequestBody accountTo: AccountTo) :ResponseEntity<Account?> {
         val account = accountService.saveAccount(accountTo)
--- src/main/java/com/webledger/webledger/controller/AllocationCenterController.kt
+++ src/main/java/com/webledger/webledger/controller/AllocationCenterController.kt
@@ -2,6 +2,7 @@ package com.webledger.webledger.controller
 
 import com.webledger.webledger.entity.AllocationCenter
 import com.webledger.webledger.service.AllocationCenterService
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,11 +14,13 @@ class AllocationCenterController(
         @Autowired
         val allocationCenterService: AllocationCenterService
 ) {
+    @ApiOperation(value = "Get a list of all allocation centers", response = AllocationCenter::class)
     @GetMapping("/allocationcenters")
     fun getAllAllocationCenters(): ResponseEntity<Iterable<AllocationCenter>?> {
         return ResponseEntity.ok(allocationCenterService.getAllAllocationCenters())
     }
 
+    @ApiOperation(value = "Get an allocation center by id", response = AllocationCenter::class)
     @GetMapping("/allocationcenters/{id}")
     fun getAllocationCenter(@PathVariable("id") id: Int): ResponseEntity<AllocationCenter?> {
         val allocationCenter = allocationCenterService.getAllocationCenter(id)
