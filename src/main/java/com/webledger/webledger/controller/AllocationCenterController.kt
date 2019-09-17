a975ac7a3534abd91e0e0ef0f7825004994332f2
Kamil Jarosz <kjarosz19946@gmail.com>
2019-09-17 08:19:57 -0400

    Add missing annotations to allocation center controller.

--- src/main/java/com/webledger/webledger/controller/AllocationCenterController.kt
+++ src/main/java/com/webledger/webledger/controller/AllocationCenterController.kt
@@ -6,12 +6,10 @@ import com.webledger.webledger.transferobject.AllocationCenterTo
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = [ "http://localhost:4200" ])
 class AllocationCenterController(
         @Autowired
         val allocationCenterService: AllocationCenterService
@@ -33,8 +31,9 @@ class AllocationCenterController(
         }
     }
 
-    @PostMapping("/allocationcenters")
-    fun saveAllocationCenter(allocationCenterTo: AllocationCenterTo): ResponseEntity<AllocationCenter?> {
+    @ApiOperation(value = "Save allocation center", response = AllocationCenter::class)
+    @PostMapping("/allocationcenters/save")
+    fun saveAllocationCenter(@RequestBody allocationCenterTo: AllocationCenterTo): ResponseEntity<AllocationCenter?> {
         return ResponseEntity.ok(allocationCenterService.saveAllocationCenter(allocationCenterTo))
     }
 }
