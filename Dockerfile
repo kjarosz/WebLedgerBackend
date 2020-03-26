1192a22b332c707116b15c593ac3ba7ef82c76bc
Kamil Jarosz <kjarosz19946@gmail.com>
2020-03-26 17:46:26 -0400

    Run in prod profile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE
+ARG ACTIVE_PROFILE
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 ./gradlew clean build
+
 docker build --build-arg JAR_FILE=build/libs/*.jar -t kjarosz/webledgerbackend .
-docker run --network=host --rm -d kjarosz/webledgerbackend
+
+docker run \
+  --network=webledger \
+  -p 8080:8080 \
+  -e "SPRING_PROFILES_ACTIVE=prod" \
+  --rm \
+  -d \
+  kjarosz/webledgerbackend
