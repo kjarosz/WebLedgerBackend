09de66ee86fd70e468f31011bd1a264f7aace195
Kamil Jarosz <kjarosz19946@gmail.com>
2020-03-27 16:17:22 -0400

    Add DNS alias for docker container

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ docker build --build-arg JAR_FILE=build/libs/*.jar -t kjarosz/webledgerbackend .
 docker run \
   --name webledger
   --network=webledger \
+  --network-alias=webledgerbackend \
   -p 8080:8080 \
   -e "SPRING_PROFILES_ACTIVE=prod" \
   --rm \
