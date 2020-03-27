ffd844e59fde6cb5a44b9d0b6bf6bc6e41adb3a9
Kamil Jarosz <kjarosz19946@gmail.com>
2020-03-27 14:37:05 -0400

    Name container

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 docker build --build-arg JAR_FILE=build/libs/*.jar -t kjarosz/webledgerbackend .
 
 docker run \
+  --name webledger
   --network=webledger \
   -p 8080:8080 \
   -e "SPRING_PROFILES_ACTIVE=prod" \
